b8e7ad603d054c253a6118743c2f4769843c77cf
Jose Rodriguez <jose.rodriguez2962@alumnos.udg.mx>
2023-03-24 22:53:51 -0600

    Creacion de modelo y controlador de Estatus y Trabajador Cadena de conexion hacioa la BD Creacion de la clase Query

---
+++ MVC-Maca_Co/Models/Estatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Maca_Co.Models
+{
+    public class Estatus
+    {
+        public int ID { get; set; }
+        public string Estatus { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ MVC-Maca_Co/Models/Trabajador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Maca_Co.Models
+{
+    public class Trabajador
+    {
+        public int ID { set; get; }
+        public string Nombre {set; get; }
+        public string Rol { get; set; }
+        public int IDUsiario { get; set; }
+
+    }
+}
